e2b90bcc224f2464779904c65efb0117a96beab5
Andras Mihaly <andrasm96@gmail.com>
2018-11-19 13:47:04 -0800

    Added d3 data visualization

--- main.js
+++ main.js
@@ -4,8 +4,15 @@ $(updateView)
 
 function updateView() {
     $.getJSON(BASE_URL + "/rides/count", (UpdateRideCount))
+    $.getJSON(BASE_URL + "/rides/count/per_month", (UpdateMonthlyCount))
 }
+
 function UpdateRideCount(data) {
     numberofRides = data.count
     $("h2#rideCount").html(numberofRides)
 }
+
+function UpdateMonthlyCount(data) {
+    console.log(data)
+}
+
